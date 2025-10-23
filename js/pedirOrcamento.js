6b422b24f2d661a9deef3a4c8e7a4aa6676cb5e1
Rodrigo Silva <rodrigoalexandre388@gmail.com>
2025-10-23 22:34:26 +0100

    Atualiza código: [Mudanças no pedido de orçamentos]

--- js/pedirOrcamento.js
+++ js/pedirOrcamento.js
@@ -1,22 +1,18 @@
-// ../js/pedirOrcamento.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-// Config real do teu projeto
 const firebaseConfig = {
     apiKey: "AIzaSyDdYedFEmv9A-zZiJcIxPllzFuiNNKTysE",
     authDomain: "ctra-ee456.firebaseapp.com",
     projectId: "ctra-ee456",
-    storageBucket: "ctra-ee456.appspot.com", // <- corrige para appspot.com
+    storageBucket: "ctra-ee456.appspot.com",
     messagingSenderId: "508055571269",
     appId: "1:508055571269:web:7193a345cf6b8741c6cc2c"
 };
 
-// Inicializar Firebase + Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Listener do formulário + gravação
 document.getElementById('orcamentoForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
